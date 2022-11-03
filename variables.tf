e62e2f7b5d349c7269621c459096717837d373c5
Kseniia Ryuma <kseniya0213@gmail.com>
2022-11-03 15:00:17 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,5 +5,4 @@ variable "SNOWFLAKE_USER" {}
 variable "SNOWFLAKE_PASSWORD" {}
 variable "SNOWFLAKE_REGION" {}
 variable "snowflake_key" {}
-variable "organization_id" {}
 variable "ASTRA_API_TOKEN" {}
